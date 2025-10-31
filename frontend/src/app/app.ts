21c846aa324744127c0eb32a3e625a05de86162f
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-10-31 12:28:19 +0800

    Connected Node.js to Angular
    Connected Node.js backend to Angular frontend. Used example from W3Schools.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './services/user';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit{
+  users: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
 }
