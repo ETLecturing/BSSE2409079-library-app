de47c056709296d2c397bc0086430b8d00c0fd0e
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-10 11:40:36 +0800

    Update Home Pahe UI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
+import { HomePage } from './home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/landing', pathMatch: 'full' },
+    { path: '/landing', component: LandingPage },
+    { path: '/home', component: HomePage }
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { UserService } from './services/user';
 
 import { LandingPage } from "./landing-page/landing-page";
+import { HomePage } from './home-page/home-page';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPage],
+  imports: [RouterOutlet, LandingPage, HomePage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
