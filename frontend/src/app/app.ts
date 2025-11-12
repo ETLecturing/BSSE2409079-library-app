2ec7292ca604b42b8d2a75950de02285606a94b7
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-12 15:56:36 +0800

    Added dynamically generating book list

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserService } from './services/user';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +7,4 @@ import { UserService } from './services/user';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
-  users: any[] = [];
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit(): void {
-    this.userService.getUsers().subscribe(data => {
-      this.users = data;
-    });
-  }
-}
+export class App{}
--- frontend/src/app/home-page/home-page.ts
+++ frontend/src/app/home-page/home-page.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Book {
+  title: string;
+  author: string;
+  publisher: string;
+  year: number;
+  genre: string;
+  language: string;
+  imgUrl: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +19,26 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private apiUrl = 'http://localhost:3000/book/api/getAll';
+  books: Book[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.http.get<Book[]>(this.apiUrl).subscribe({
+      next: (data) => {
+        this.books = data;
+        console.log('Books loaded:', this.books);
+      },
+      error: (error) => {
+        console.log('Error fetching books:', error);
+      }
+    });
+    
+  }
 }
