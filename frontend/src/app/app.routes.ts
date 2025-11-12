fe48e6f9c0049d0a9ec2563282892556458fd311
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-12 17:15:07 +0800

    Create Booking Page & Route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
 import { HomePage } from './home-page/home-page';
 import { AddBookPage } from './add-book-page/add-book-page';
+import { BookingPage } from './booking-page/booking-page';
 
 export const routes: Routes = [
     { path: 'landing', component: LandingPage },
     { path: '', redirectTo: 'landing', pathMatch: 'full' },
     { path: 'home', component: HomePage },
-    { path: 'add-book', component: AddBookPage }
+    { path: 'add-book', component: AddBookPage },
+    { path: 'booking', component: BookingPage}
 ];
