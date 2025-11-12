886c63e35bf67fc5000654be3eb149f5da3b0110
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-12 14:32:26 +0800

    Create page to add books

---
+++ frontend/src/app/add-book-page/add-book-page.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-add-book-page',
+  imports: [ReactiveFormsModule],
+  templateUrl: './add-book-page.html',
+  styleUrl: './add-book-page.css'
+})
+export class AddBookPage {
+
+  addBookForm: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    author: new FormControl(''),
+    publisher: new FormControl(''),
+    year: new FormControl(0),
+    genre: new FormControl(''),
+    language: new FormControl(''),
+    imgUrl: new FormControl(''),
+    status: new FormControl('')
+  });
+
+  constructor(private http: HttpClient) {}
+
+  addBook(event: Event) {
+    event.preventDefault();
+
+    console.log('Form is submitted!')
+    console.log(this.addBookForm.value)
+
+    if(this.addBookForm.valid) {
+      this.http.post('http://localhost:3000/book/api/add', this.addBookForm.value)
+      .subscribe({
+        next: response => console.log('Book added', response),
+        error: error => console.log('Error', error),
+        complete: () => console.log('Complete')
+      });
+    }
+  }
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
 import { HomePage } from './home-page/home-page';
+import { AddBookPage } from './add-book-page/add-book-page';
 
 export const routes: Routes = [
     { path: 'landing', component: LandingPage },
     { path: '', redirectTo: 'landing', pathMatch: 'full' },
-    { path: 'home', component: HomePage }
+    { path: 'home', component: HomePage },
+    { path: 'add-book', component: AddBookPage }
 ];
