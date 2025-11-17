79afebd9cd42c00783c30a7786dd6de283170338
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-18 00:59:33 +0800

    Initialise Transaction Controllers & Routes

---
+++ backend/src/controllers/transactionController.js
@@ -0,0 +1,21 @@
+const Transaction = require('../models/Transaction');
+const Reservation = require('../models/Reservation');
+const Booking = require('../models/Booking');
+
+async function createReservation(req, res) {
+    // get today's date
+    // create a Reservation of the book, enum 'reserve'
+}
+
+async function deleteReservation(req, res) {
+    // un-reserve a book
+}
+
+async function createBooking(req, res) {
+    // create booking
+    // certain values are null
+}
+
+async function deleteBooking(req, res) {
+    // home-page
+}
