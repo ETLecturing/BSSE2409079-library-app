e412de59a1006f621e88578fbc8148d6176ab361
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-20 17:17:26 +0800

    Update auth-service.ts

--- frontend/src/app/services/auth-service.ts
+++ frontend/src/app/services/auth-service.ts
@@ -1,19 +1,49 @@
 import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private tokenKey = 'token';
+  private _decodedToken: any = null;
+
+  constructor(private router: Router) {
+    const token = this.getToken();
+    if (token) this._decodedToken = this.decodeToken(token);
+  }
   
-  saveToken(token: string) {
-    localStorage.setItem('token', token);
+  saveToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+    this._decodedToken = this.decodeToken(token);
   }
 
-  getToken() {
-    return localStorage.getItem('token');
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
   }
 
-  logout() {
-    localStorage.removeItem('token');
+  logOut(): void {
+    this.router.navigate(['/landing']);
+    localStorage.removeItem(this.tokenKey);
+    this._decodedToken = null;
   }
+
+  private decodeToken(token: string): any {
+    try {
+      return jwtDecode(token);
+    } catch (err) {
+      console.error('Invalid token', err);
+      return null;
+    }
+  }
+
+  get memberId(): string | null {
+    return this._decodedToken?.memberId || null;
+  }
+
+  get memberName(): string | null {
+    return this._decodedToken?.memberName || null;
+  }
+
 }
