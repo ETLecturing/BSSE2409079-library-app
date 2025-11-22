609cfd86ea9348ffbcec2cded897f90498b7fde7
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-22 18:52:46 +0800

    Update server.js
    - added socket.io Server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { connectDB } = require('./src/db');
 const http = require('http');
 const app = require('./src/app');
+const { Server } = require('socket.io');
 
 const hostname = '0.0.0.0'
 const port = process.env.PORT || 3000;
@@ -11,6 +12,12 @@ const port = process.env.PORT || 3000;
     await connectDB();
 
     const server = http.createServer(app);
+
+    const io = new Server(server, {
+        cors: { origin: "*", methods: ["GET", "POST"] }
+    });
+
+    app.set('io', io);
 
     server.listen(port, hostname, function() {
         console.log(`Server listening on http://${hostname}:${port}/`);
