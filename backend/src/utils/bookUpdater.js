4c0e6ea3d869165833dea9b99107071027088891
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-22 21:03:51 +0800

    Update bookUpdater.js
    - removed console.log
    - added a try catch block
    - added to check if book exists before changing its status

--- backend/src/utils/bookUpdater.js
+++ backend/src/utils/bookUpdater.js
@@ -3,22 +3,29 @@ const Reservation = require('../models/Reservation');
 const Booking = require('../models/Booking');
 
 async function updateBookStatus(bookId) {
+    try {
+        const book = await Book.findById(bookId);
+        if (!book) {
+            throw new Error(`Book with _id: ${bookId} cannot be found.`);
+        }
 
-    const borrow = await Booking.findOne({ bookId, returnDate: null });
-    if (borrow) {
-        await Book.updateOne({ _id: bookId }, { status: 'borrowed' });
-        console.log('book status changed to borrowed');
-        return;
+        const borrow = await Booking.findOne({ bookId, returnDate: null });
+        if (borrow) {
+            await Book.updateOne({ _id: bookId }, { status: 'borrowed' });
+            return;
+        }
+
+        const reservation = await Reservation.findOne({ bookId });
+        if (reservation) {
+            await Book.updateOne({ _id: bookId }, { status: 'reserved' });
+        }
+
+        await Book.updateOne({ _id: bookId }, { status: 'available' });
+        
+    } catch (error) {
+        console.log('Database error', error);
+        throw error;
     }
-
-    const reservation = await Reservation.findOne({ bookId });
-    if (reservation) {
-        await Book.updateOne({ _id: bookId }, { status: 'reserved' });
-        console.log('book status changed to reserved');
-        return;
-    }
-
-    await Book.updateOne({ _id: bookId }, { status: 'available' });
 }
 
 module.exports = { updateBookStatus };
