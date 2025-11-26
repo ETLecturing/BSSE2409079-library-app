836ace1947deb48596bdc8f9a944d67bd1682fb2
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-26 14:45:30 +0800

    Update environment API urls

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    apiUrl: 'http://192.168.103:3000'
-    // apiUrl: 'http://localhost:3000'
+    // apiUrl: 'http://172.18.52.39:3000' // TSC WiFi IP
+    apiUrl: 'http://192.168.103:3000' // Home WiFi IP
+    // apiUrl: 'http://localhost:3000' // Localhost
 };
