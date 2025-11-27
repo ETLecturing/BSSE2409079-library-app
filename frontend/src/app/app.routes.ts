006e96c0a9e62955a6ed3ee7e72b2be32fb05150
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-11-27 15:49:43 +0800

    Add Guard Auth to Angular
    - prevent user from accessing sites that does not exist
    - prevent non-member from trying to access home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingPage } from './landing-page/landing-page';
 import { HomePage } from './home-page/home-page';
 import { AddBookPage } from './add-book-page/add-book-page';
 import { BookingPage } from './booking-page/booking-page';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     { path: 'landing', component: LandingPage },
     { path: '', redirectTo: 'landing', pathMatch: 'full' },
-    { path: 'home', component: HomePage },
-    { path: 'add-book', component: AddBookPage },
-    { path: 'booking/:id', component: BookingPage}
+    { path: 'home', canActivate: [authGuard], component: HomePage },
+    { path: 'add-book', canActivate: [authGuard], component: AddBookPage },
+    { path: 'booking/:id', canActivate: [authGuard], component: BookingPage},
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
